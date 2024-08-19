cdfb265fad711cb23c0864cbc61fb4ff6e7d41b7
0xhaz <28639279+0xhaz@users.noreply.github.com>
2024-08-20 00:58:08 +0800

    Lesson 7

--- src/Tutorial/LaMerde/LaMerdeHooks.sol
+++ src/Tutorial/LaMerde/LaMerdeHooks.sol
@@ -3,18 +3,32 @@ pragma solidity ^0.8.25;
 
 import {BaseHook} from "v4-periphery/src/base/hooks/BaseHook.sol";
 import {ERC20} from "solmate/src/tokens/ERC20.sol";
-import {CurrencyLibrary, Currency} from "v4-core/types/Currency.sol";
-import {PoolKey} from "v4-core/types/PoolKey.sol";
-import {BalanceDeltaLibrary, BalanceDelta} from "v4-core/types/BalanceDelta.sol";
 import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
 import {Hooks} from "v4-core/libraries/Hooks.sol";
+import {BalanceDeltaLibrary, BalanceDelta} from "v4-core/types/BalanceDelta.sol";
+import {CurrencyLibrary, Currency} from "v4-core/types/Currency.sol";
+import {PoolKey} from "v4-core/types/PoolKey.sol";
+import {PoolId, PoolIdLibrary} from "v4-core/types/PoolId.sol";
+import {StateLibrary} from "v4-core/libraries/StateLibrary.sol";
+import {SafeCast} from "v4-core/libraries/SafeCast.sol";
+import {FullMath} from "v4-core/libraries/FullMath.sol";
+import {LiquidityMath} from "v4-core/libraries/LiquidityMath.sol";
+import {SwapMath} from "v4-core/libraries/SwapMath.sol";
+import {FixedPoint128} from "v4-core/libraries/FixedPoint128.sol";
+import {ProtocolFeeLibrary} from "v4-core/libraries/ProtocolFeeLibrary.sol";
+import {Slot0} from "v4-core/types/Slot0.sol";
+import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "v4-core/types/BeforeSwapDelta.sol";
+import {CurrencyDelta} from "v4-core/libraries/CurrencyDelta.sol";
 
-contract PointsProgramHook is BaseHook, ERC20 {
+contract LaMerdeHooks is BaseHook, ERC20 {
     using CurrencyLibrary for Currency;
     using BalanceDeltaLibrary for BalanceDelta;
+    using PoolIdLibrary for PoolKey;
+    using SafeCast for uint256;
+    using StateLibrary for IPoolManager;
 
     mapping(address => address) public referredBy;
-    uint256 public constant POINTS_FOR_REFERRAL = 500 * 10 ** 18;
+    uint256 public constant POINTS_FOR_REFERRAL = 500e18;
 
     constructor(IPoolManager _manager, string memory _name, string memory _symbol)
         BaseHook(_manager)
@@ -48,42 +62,53 @@ contract PointsProgramHook is BaseHook, ERC20 {
         bytes calldata hookData
     ) external override onlyByPoolManager returns (bytes4, int128) {
         if (!key.currency0.isNative()) return (this.afterSwap.selector, 0);
-        if (swapParams.zeroForOne) return (this.afterSwap.selector, 0);
 
+        // We only mint points if user is buying TOKEN with ETH
+        if (!swapParams.zeroForOne) return (this.afterSwap.selector, 0);
+
+        uint256 ethSpendAmount =
+            swapParams.amountSpecified < 0 ? uint256(-swapParams.amountSpecified) : uint256(int256(-delta.amount0()));
+        uint256 pointsForSwap = ethSpendAmount / 5;
+
+        _assignPoints(hookData, pointsForSwap);
         return (this.afterSwap.selector, 0);
     }
 
     function afterAddLiquidity(
         address,
         PoolKey calldata key,
-        IPoolManager.ModifyLiquidityParams calldata,
+        IPoolManager.ModifyLiquidityParams calldata params,
         BalanceDelta delta,
         bytes calldata hookData
     ) external override onlyByPoolManager returns (bytes4, BalanceDelta) {
         return (this.afterAddLiquidity.selector, delta);
     }
 
-    //////////////////////////// Helpers ////////////////////////////
-    function getHookData(address referrer, address referree) public view returns (bytes memory) {
+    ///////////////////////////// Helper Functions /////////////////////////////
+
+    function getHookData(address referrer, address referree) public pure returns (bytes memory) {
         return abi.encode(referrer, referree);
     }
 
-    function _assignPoints(bytes calldata hookData, uint256 referreePoints) internal {
+    function _assignPoints(bytes calldata hookData, uint256 referralPoints) internal {
         if (hookData.length == 0) return;
 
         (address referrer, address referree) = abi.decode(hookData, (address, address));
 
-        if (referree == address(0)) return;
-
+        // Assign points for referral
+        // if referree is being transferred by someone for the first time
+        // and mint points for referrer
         if (referredBy[referree] == address(0) && referrer != address(0)) {
             referredBy[referree] = referrer;
             _mint(referrer, POINTS_FOR_REFERRAL);
         }
 
+        // Mint 10% worth of referree points to referrer
         if (referredBy[referree] != address(0)) {
-            _mint(referree, referreePoints / 10);
+            _mint(referredBy[referree], referralPoints / 10);
         }
 
-        _mint(referree, referreePoints);
+        // Mint points for referree
+        _mint(referree, referralPoints);
     }
 }
